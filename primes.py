1ae879803308561eba00cfd2fc62c746bb6f3022
Susie James <susetta.james@kcl.ac.uk>
2022-10-02 22:01:14 +0100

    program finished

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+    num = 2
+    count = 1
     list = []
+    while count <= number_of_primes:
+        flag = False
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = True
+                break
+        if not flag:
+            list.append(num)
+            count += 1
+        num += 1
+
     return list
+
